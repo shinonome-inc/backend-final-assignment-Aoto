ac6e721f8fef2fa1df26691cafb8bf3a2dae7839
aotom <aotonabe0606@gmail.com>
2023-06-24 21:36:00 +0900

    １次レビュー修正

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    email = models.EmailField()
+    email = models.EmailField("email address", unique=True)
 
 
 # class FriendShip(models.Model):
